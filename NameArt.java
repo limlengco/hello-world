e50d77d9f42a25e4c26fc7c8897f4e064dc91677
limlengco <31555816+limlengco@users.noreply.github.com>
2017-09-12 20:18:04 -0400

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,63 @@
+//* Kevin Limlengco
+//* CMSC 255
+//* Project 1 "Your name in Code"
+public class NameArt{ 
+  public static void main(String[] args) {
+//* Prints out name class project name and project no.
+    System.out.println("Kevin Limlengco");
+    System.out.println("CMSC 255 Section 001");
+    System.out.println("NameArt.java");
+    System.out.println("Project 1");
+ //* Prints out my name in huge ascii format   
+    System.out.println
+  (" ****      ****    **************    ****        ****    ****    ****      ****");
+    System.out.println
+  (" ****      ****    **************    ****        ****    ****    *****     ****");
+    System.out.println
+  (" ****      ****    **************    ****        ****    ****    *****     ****");
+    System.out.println
+  (" ****     ****     **************    *****      *****    ****    *****     ****");
+    System.out.println
+  (" ****     ****     ******             ****      ****     ****    ******    ****");
+    System.out.println
+  (" ****    ****      ******              ****    ****      ****    *******   ****");
+    System.out.println
+  (" ****   ****       *************        ****  ****       ****    *******   ****");
+    System.out.println
+  (" ****   ****       *************         ***  ***        ****    *******   ****");
+    System.out.println
+  (" ****   ***        *************          **  **         ****    ********  ****");
+    System.out.println
+  (" ********          ******                   * *          ****    ********* ****");
+    System.out.println
+  (" *******           ******                    *           ****    **************");
+    System.out.println
+  (" *******           *************                                 **************");
+    System.out.println
+  (" ********          *************                                 **************");
+    System.out.println
+  (" ****   ***        *************                                 **************");
+    System.out.println
+  (" ****   ****                                                     **************");
+    System.out.println
+  (" ****   ****                                                     **************");
+    System.out.println
+  (" ****    ****                                                    **************");
+    System.out.println
+  (" ****    ****                                                    **** *********");
+    System.out.println
+  (" ****     ****                                                   ****  ********");
+    System.out.println
+  (" ****     ****                                                   ****  ********");
+    System.out.println
+  (" ****      ****                                                  ****   *******");
+    System.out.println
+  (" ****      ****                                                  ****    ******");
+    System.out.println
+  (" ****       ****                                                 ****     *****");
+    System.out.println
+  (" ****       ****                                                 ****     *****");
+    System.out.println
+  (" ****       ****                                                 ****     *****");}
+}
+    
